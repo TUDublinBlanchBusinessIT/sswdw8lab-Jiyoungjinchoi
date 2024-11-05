cca19216cc3bda116f409ce6c7138616f845b8f7
B00158376 Jiyoung Choi <B00158376@mytudublin.ie>
2024-11-05 16:02:27 +0000

    Part 2 completed

---
+++ CarPolicy2.php
@@ -0,0 +1,50 @@
+<?php
+
+class CarPolicy
+{
+    private $policyNumber="";
+    private $yearlyPremium="";
+    private $dateOfLastClaim="";
+
+    public function __construct($policyNumber, $yearlyPremium) 
+    {
+        $this->policyNumber = $policyNumber;
+        $this->yearlyPremium = $yearlyPremium;
+    }
+    public function setDateOfLastClaim($dateOfLastClaim) 
+    {
+        $this->dateOfLastClaim = $dateOfLastClaim;
+    }
+    public function getTotalYearsNoClaims() 
+    {
+        $currentDate = new DateTime(); 
+        $lastDate = new DateTime($this->dateOfLastClaim); 
+        $interval = $currentDate->diff($lastDate); 
+        return $interval->format("%y"); 
+    }
+    public function getDiscount() 
+    {
+            $yearsNoClaims = $this->getTotalYearsNoClaims();
+    
+            if ($yearsNoClaims >= 3 && $yearsNoClaims <= 5) {
+                return 0.10; 
+            } elseif ($yearsNoClaims > 5) {
+                return 0.15;
+            }
+    
+            return 0; 
+    }
+    public function getDiscountedPremium() 
+    {
+            $discount = $this->getDiscount();
+            return $this->yearlyPremium * (1 - $discount); 
+    }
+    
+    public function __toString() 
+    {
+        return "PN: " . $this->policyNumber;
+    }
+}
+
+
+?>
